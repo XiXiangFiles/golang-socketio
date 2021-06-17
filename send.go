79a0377c1fdf2f65472ad88a97845548dc39e2e0
jerry.wang <jerry.wang@rogersai.com>
2021-06-17 18:53:51 +0800

    fixed can't receive message bug

--- send.go
+++ send.go
@@ -3,9 +3,10 @@ package gosocketio
 import (
 	"encoding/json"
 	"errors"
-	"github.com/graarh/golang-socketio/protocol"
 	"log"
 	"time"
+
+	"github.com/XiXiangFiles/golang-socketio/protocol"
 )
 
 var (
